4576b6870813bc995c8170d8896fc17be06808a9
DevExpressExampleBot <fakeEmail>
2025-03-14 13:51:51 +0400

    Source auto update [skip ci]

--- VB/MainWindow.xaml.vb
+++ VB/MainWindow.xaml.vb
@@ -1,17 +1,19 @@
-﻿Imports System.Windows
+Imports System.Windows
 
 Namespace DXMapLesson2
-	''' <summary>
-	''' Interaction logic for MainWindow.xaml
-	''' </summary>
-	Partial Public Class MainWindow
-		Inherits Window
 
-		Public Sub New()
-			InitializeComponent()
-		End Sub
-		Private Sub OnWebRequest(ByVal sender As Object, ByVal e As DevExpress.Xpf.Map.MapWebRequestEventArgs)
-			e.UserAgent = "DevExpress OpenStreetMapProvider example"
-		End Sub
-	End Class
+    ''' <summary>
+    ''' Interaction logic for MainWindow.xaml
+    ''' </summary>
+    Public Partial Class MainWindow
+        Inherits Window
+
+        Public Sub New()
+            Me.InitializeComponent()
+        End Sub
+
+        Private Sub OnWebRequest(ByVal sender As Object, ByVal e As DevExpress.Xpf.Map.MapWebRequestEventArgs)
+            e.UserAgent = "DevExpress OpenStreetMapProvider example"
+        End Sub
+    End Class
 End Namespace
