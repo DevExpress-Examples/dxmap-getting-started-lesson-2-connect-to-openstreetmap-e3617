afd1f02a0ed910c9c3557208a84f6f9a2539f843
DevExpressExampleBot <fakeEmail>
2025-03-14 13:47:28 +0400

    Source auto update [skip ci]

--- VB/MainWindow.xaml.vb
+++ VB/MainWindow.xaml.vb
@@ -1,14 +1,15 @@
-﻿Imports Microsoft.VisualBasic
 Imports System.Windows
 
 Namespace DXMapLesson2
-	''' <summary>
-	''' Interaction logic for MainWindow.xaml
-	''' </summary>
-	Partial Public Class MainWindow
-		Inherits Window
-		Public Sub New()
-			InitializeComponent()
-		End Sub
-	End Class
+
+    ''' <summary>
+    ''' Interaction logic for MainWindow.xaml
+    ''' </summary>
+    Public Partial Class MainWindow
+        Inherits Window
+
+        Public Sub New()
+            Me.InitializeComponent()
+        End Sub
+    End Class
 End Namespace
